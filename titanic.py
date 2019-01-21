58d0f78addb77022abbcf8cae135810022406cd8
PratibhaGaikwad <46512675+PratibhaGaikwad@users.noreply.github.com>
2019-01-21 10:11:19 +0530

    Create titanic.py

---
+++ titanic.py
@@ -0,0 +1,451 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
+
+
+# In[7]:
+
+
+train = pd.read_csv("train.csv")
+
+
+# In[8]:
+
+
+test = pd.read_csv("test.csv")
+
+
+# In[12]:
+
+
+train.head()
+
+
+# In[13]:
+
+
+test.head()
+
+
+# In[14]:
+
+
+train_test_data = [train, test] # combining train and test dataset
+
+for dataset in train_test_data:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.')
+
+
+# In[15]:
+
+
+train.head()
+
+
+# In[16]:
+
+
+pd.crosstab(train['Title'], train['Sex'])
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+for dataset in train_test_data:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',  	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Other')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    
+train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean()
+
+
+# In[18]:
+
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Other": 5}
+for dataset in train_test_data:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+
+# In[19]:
+
+
+train.head()
+
+
+# In[20]:
+
+
+for dataset in train_test_data:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+
+# In[21]:
+
+
+train.head()
+
+
+# In[22]:
+
+
+for dataset in train_test_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+
+
+# In[23]:
+
+
+train.head()
+
+
+# In[24]:
+
+
+for dataset in train_test_data:
+    #print(dataset.Embarked.unique())
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+
+# In[25]:
+
+
+train.head()
+
+
+# In[26]:
+
+
+for dataset in train_test_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+    
+train['AgeBand'] = pd.cut(train['Age'], 5)
+
+print (train[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean())
+
+
+# In[27]:
+
+
+train.head()
+
+
+# In[28]:
+
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+
+
+# In[29]:
+
+
+for dataset in train_test_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+
+
+# In[30]:
+
+
+train.head()
+
+
+# In[31]:
+
+
+train['FareBand'] = pd.qcut(train['Fare'], 4)
+print (train[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean())
+
+
+# In[32]:
+
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+
+# In[33]:
+
+
+train.head()
+
+
+# In[34]:
+
+
+for dataset in train_test_data:
+    dataset['FamilySize'] = dataset['SibSp'] +  dataset['Parch'] + 1
+
+print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
+
+
+# In[35]:
+
+
+for dataset in train_test_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+    
+print (train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+
+
+# In[36]:
+
+
+train.head()
+
+
+# In[37]:
+
+
+test.head()
+
+
+# In[38]:
+
+
+features_drop = ['Name', 'SibSp', 'Parch', 'Ticket', 'Cabin', 'FamilySize']
+train = train.drop(features_drop, axis=1)
+test = test.drop(features_drop, axis=1)
+train = train.drop(['PassengerId', 'AgeBand', 'FareBand'], axis=1)
+
+
+# In[39]:
+
+
+train.head()
+
+
+# In[40]:
+
+
+test.head()
+
+
+# In[41]:
+
+
+X_train = train.drop('Survived', axis=1)
+y_train = train['Survived']
+X_test = test.drop("PassengerId", axis=1).copy()
+
+X_train.shape, y_train.shape, X_test.shape
+
+
+# In[42]:
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+
+
+# In[43]:
+
+
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+y_pred_log_reg = clf.predict(X_test)
+acc_log_reg = round( clf.score(X_train, y_train) * 100, 2)
+print (str(acc_log_reg) + ' percent')
+
+
+# In[44]:
+
+
+clf = SVC()
+clf.fit(X_train, y_train)
+y_pred_svc = clf.predict(X_test)
+acc_svc = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_svc)
+
+
+# In[45]:
+
+
+clf = LinearSVC()
+clf.fit(X_train, y_train)
+y_pred_linear_svc = clf.predict(X_test)
+acc_linear_svc = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_linear_svc)
+
+
+# In[46]:
+
+
+clf = KNeighborsClassifier(n_neighbors = 3)
+clf.fit(X_train, y_train)
+y_pred_knn = clf.predict(X_test)
+acc_knn = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_knn)
+
+
+# In[47]:
+
+
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+y_pred_decision_tree = clf.predict(X_test)
+acc_decision_tree = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_decision_tree)
+
+
+# In[48]:
+
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+y_pred_random_forest = clf.predict(X_test)
+acc_random_forest = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_random_forest)
+
+
+# In[49]:
+
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+y_pred_gnb = clf.predict(X_test)
+acc_gnb = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_gnb)
+
+
+# In[50]:
+
+
+clf = Perceptron(max_iter=5, tol=None)
+clf.fit(X_train, y_train)
+y_pred_perceptron = clf.predict(X_test)
+acc_perceptron = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_perceptron)
+
+
+
+
+
+clf = SGDClassifier(max_iter=5, tol=None)
+clf.fit(X_train, y_train)
+y_pred_sgd = clf.predict(X_test)
+acc_sgd = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_sgd)
+
+
+
+from sklearn.metrics import confusion_matrix
+import itertools
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+y_pred_random_forest_training_set = clf.predict(X_train)
+acc_random_forest = round(clf.score(X_train, y_train) * 100, 2)
+print ("Accuracy: %i %% \n"%acc_random_forest)
+
+class_names = ['Survived', 'Not Survived']
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_train, y_pred_random_forest_training_set)
+np.set_printoptions(precision=2)
+
+print ('Confusion Matrix in Numbers')
+print (cnf_matrix)
+print ('')
+
+cnf_matrix_percent = cnf_matrix.astype('float') / cnf_matrix.sum(axis=1)[:, np.newaxis]
+
+print ('Confusion Matrix in Percentage')
+print (cnf_matrix_percent)
+print ('')
+
+true_class_names = ['True Survived', 'True Not Survived']
+predicted_class_names = ['Predicted Survived', 'Predicted Not Survived']
+
+df_cnf_matrix = pd.DataFrame(cnf_matrix, 
+                             index = true_class_names,
+                             columns = predicted_class_names)
+
+df_cnf_matrix_percent = pd.DataFrame(cnf_matrix_percent, 
+                                     index = true_class_names,
+                                     columns = predicted_class_names)
+
+plt.figure(figsize = (15,5))
+
+plt.subplot(121)
+sns.heatmap(df_cnf_matrix, annot=True, fmt='d')
+
+plt.subplot(122)
+sns.heatmap(df_cnf_matrix_percent, annot=True)
+
+
+
+
+models = pd.DataFrame({
+    'Model': ['Logistic Regression', 'Support Vector Machines', 'Linear SVC', 
+              'KNN', 'Decision Tree', 'Random Forest', 'Naive Bayes', 
+              'Perceptron', 'Stochastic Gradient Decent'],
+    
+    'Score': [acc_log_reg, acc_svc, acc_linear_svc, 
+              acc_knn,  acc_decision_tree, acc_random_forest, acc_gnb, 
+              acc_perceptron, acc_sgd]
+    })
+
+models.sort_values(by='Score', ascending=False)
+models
+
+
+
+
+
+test.head()
+
+
+
+
+submission = pd.DataFrame({
+        "PassengerId": test["PassengerId"],
+        "Survived": y_pred_random_forest
+    })
+
+submission.to_csv('submission.csv', index=False)
+
+
+
+
+
+
+
